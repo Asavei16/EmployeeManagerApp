a2617db67adc51283736c6338792b3f2d6a9456a
Asavei16 <asaveiflo@gmail.com>
2023-09-09 18:38:40 +0300

    First commit

---
+++ src/app/employee.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employee } from 'src/app/employee';
+import { HttpClient } from '@angular/common/http'
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+  private apiServerUrl = 'http://localhost:8080';
+
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getEmployees(): Observable<Employee[]> {
+    return this.httpClient.get<Employee[]>(`${this.apiServerUrl}/employee/all`);
+  }
+
+  public addEmployee(employee: Employee): Observable<Employee> {
+    return this.httpClient.post<Employee>(`${this.apiServerUrl}/employee/add`, employee);
+  }
+
+  public updateEmployee(employee: Employee): Observable<Employee> {
+    return this.httpClient.put<Employee>(`${this.apiServerUrl}/employee/update`, employee);
+  }
+
+  public deleteEmployee(employeeId: number): Observable<void> {
+    return this.httpClient.delete<void>(`${this.apiServerUrl}/employee/delete/${employeeId}`);
+  }
+}
